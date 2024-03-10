c7774caaf8ce9afafdd443fdcd2df110f45ffca1
Wallace Benites <148720798+wallaceeBenites@users.noreply.github.com>
2024-03-10 11:03:06 -0300

    Spring security e tokens

---
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -0,0 +1,39 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.DadosAutenticacao;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
+
+        var Authenticationtoken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var autenticacao = manager.authenticate(Authenticationtoken);
+
+        var tokenJWT = tokenService.gerarToken((Usuario) autenticacao.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
+
+
+
+
+}
